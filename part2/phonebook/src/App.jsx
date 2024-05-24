b548fcfd30255f42c9868a33548d6468824d36c7
cryogonal <HUiCOMiCS@gmail.com>
2024-05-23 23:08:06 -0400

    Finished part 2d

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from 'react'
+//import axios from 'axios'
+import nameService from './services/name'
 
 
-const Names = ({person}) => {
+const Names = ({person, removePerson}) => {
   return (
     <div>
-      {person.name} {person.number}
+      {person.name} {person.number} <button onClick = {() => removePerson(person.id, person.name)}>delete</button>
     </div>
   )
 }
@@ -35,41 +37,93 @@ const PersonForm = ({addPerson, newName, handlePersonChange, newNumber, handleNu
   )
 } 
 
-const Persons = ({filter}) => {
+const Persons = ({filter, removePerson}) => {
   return (
     <div>
       {filter.map((person) => {
-          return <Names key = {person.name} person = {person} />
+          return <Names key = {person.name} person = {person} removePerson = {removePerson}/>
         })}
     </div>
   )
 }
 
-const App = (props) => {
-  const [persons, setPersons] = useState(props.persons)
+const App = () => {
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchPerson, setSearchPerson] = useState('')
-  const [filter, setFilter] = useState(props.persons)
+  const [filter, setFilter] = useState([])
 
+  const hook = () => {
+    console.log('effect')
+    nameService.getAll()
+      .then(initialPerson => {
+        console.log("promise fulfilled")
+        setPersons(initialPerson)
+        setFilter(initialPerson)
+      })
+      .catch(error => {
+        console.error("error fetching data: ", error)
+      })
+  }
+  useEffect(hook, [])
+  console.log("rendered", persons.length, "persons")
   
   const addPerson = (event) => {
     event.preventDefault()
+
+    const nameExists = persons.find((person) => person.name.toLowerCase() === newName.toLowerCase())
 
     const personObject = {
       name: newName,
       number: newNumber,
     }
 
-    if (persons.map((person) => person.name).includes(newName)) {
-      alert(`${newName} is already added to phonebook`);
-      return;
-    }
+    if (nameExists) {
+      const confirmedName = window.confirm(`${newName} is already added to phonebook, replace the old one with a new one?`);
 
-    setPersons(filter.concat(personObject))
-    setFilter(filter.concat(personObject))
+      if (!confirmedName) {
+        return;
+      }
+      nameService.update(nameExists.id, personObject).then(updatedPerson => {
+          setPersons(prevPerson => {
+            prevPerson.id === nameExists.id ? updatedPerson : persons
+          })
+          setFilter(prevFilter => {
+            prevFilter.id === nameExists.id ? updatedPerson : persons
+          })
+        })
+        .catch(error => {
+          console.log("Error updating the number", error.message)
+          alert("Error updating the number")
+        })
+    }
+    else {
+        nameService.create(personObject)
+          .then(returnedPerson => {
+          console.log(returnedPerson)
+          setPersons(persons.concat(returnedPerson))
+          setFilter(filter.concat(returnedPerson))
+      })
+    }
     setNewName('')
     setNewNumber('')
+  }
+
+  const removePerson = (id, name) => {
+  const confirmDelete = window.confirm(`Do you want to remove ${name}?`)
+  if (!confirmDelete) {
+    return;
+  }
+    nameService.remove(id)
+      .then(() => {
+        setPersons(persons.filter(person => person.id !== id))
+        setFilter(filter.filter(person => person.id !== id))
+      })
+      .catch(error => {
+        console.log('error deleting person', error.message)
+        alert('error deleting person')
+      })
   }
 
   const handlePersonChange = (event) => {
@@ -105,7 +159,7 @@ const App = (props) => {
         handleNumberChange = {handleNumberChange}
       />
       <h2>Numbers</h2>
-      <Persons filter = {filter} />
+      <Persons filter = {filter} removePerson = {removePerson}/>
     </div>
   )
 }
